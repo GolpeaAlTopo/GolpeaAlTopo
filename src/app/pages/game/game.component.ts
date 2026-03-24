c91f48cb01ebea6bf2894c2f3dec0020f24e7f1b
Estanislao Pan García <vn.estanislao.pan@stlexternals.com>
2026-03-24 17:35:09 +0100

    Base de componente Game

---
+++ src/app/pages/game/game.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  imports: [],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss',
+  standalone: true
+})
+export class GameComponent {}
