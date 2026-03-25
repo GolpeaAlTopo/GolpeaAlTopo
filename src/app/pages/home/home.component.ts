c1da29573f60ee2ec421388e1ec4d9333f664520
Estanislao Pan García <vn.estanislao.pan@stlexternals.com>
2026-03-25 22:16:07 +0100

    Componente Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true
 })
 export class HomeComponent {
+  name = '';
+  error = '';
 
   constructor(private router: Router) {
-    this.router = router;
+    
   }
-  name = '';
   
 
   startGame() {
     if (!this.name.trim()) {
+      this.error = 'Introduce un nombre válido';
       return
     };
+    this.error = '';
     this.router.navigate(['/game'], { state: { name: this.name } });
   }
 }
