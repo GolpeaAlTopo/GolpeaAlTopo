26c7337a7995c7ff36871b420d21cf5f730ba13a
Estanislao Pan García <vn.estanislao.pan@stlexternals.com>
2026-03-27 00:31:10 +0100

    Linteo AppRoutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { GameComponent } from './pages/game/game.component';
-import { RouterOutlet } from '@angular/router';
 
 
 export const routes: Routes = [
